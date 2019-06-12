2119ebe35c0a4ebd6cfd47e0c72beb2a34306d67
ZMM <zhang163mingmin@163.com>
2019-06-12 11:27:41 +0800

    配置文件修改

--- vue.config.js
+++ vue.config.js
@@ -1,36 +1,36 @@
 const CompressionPlugin = require('compression-webpack-plugin')
 
-module.exports = (() => {
-  let option = {
-    publicPath: '/',
-    outputDir: './docker/dist',
-    lintOnSave: true,
-    runtimeCompiler: false,
-    productionSourceMap: false,
-    configureWebpack: {
-      plugins: [
-        new CompressionPlugin({
-          asset: '[path].gz[query]',
-          algorithm: 'gzip',
-          test: /\.js$|\.css$/,
-          threshold: 10240,
-          minRatio: 0.8
-        })
-      ]
+module.exports = {
+  baseUrl: '/',
+  outputDir: './docker/dist',
+  lintOnSave: true,
+  runtimeCompiler: false,
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      // 构建过程中启用gzip压缩
+      new CompressionPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$/,
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    ]
+  },
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8888,
+    https: false,
+    hotOnly: false,
+    // 配置代理转发
+    proxy: {
+      '/SearchCase': {
+        target: 'http://192.168.22.63:8081',
+        changeOrigin: true
+      }
     },
-    devServer: {
-      open: process.platform === 'darwin',
-      host: '0.0.0.0',
-      port: 8888,
-      https: false,
-      hotOnly: false,
-      proxy: {},
-      before: app => {}
-    }
+    before: app => {}
   }
-  option.devServer.proxy[`${process.env.LOCATION}`] = {
-    target: process.env.TARGET,
-    changeOrigin: true
-  }
-  return option
-})()
+}
