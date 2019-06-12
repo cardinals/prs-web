756843ec243e809f9a8a7418d2ea4b99e8d7b76f
ZMM <zhang163mingmin@163.com>
2019-06-12 15:25:37 +0800

    fix: 脚部公共组件代码重构

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -1,17 +1,5 @@
-/*
- * @Author: wupeiwen javapeiwen2010@gmail.com
- * @Date: 2018-09-18 10:01:39
- * @Last Modified by: wupeiwen javapeiwen2010@gmail.com
- * @Last Modified time: 2018-09-18 11:00:18
- */
-
 <template>
   <div class="footer" :class="{'footer2':$route.fullPath==='/login'||$route.fullPath==='/home'}">
-    <!-- <div v-if="$route.fullPath!=='/login'&&$route.fullPath!=='/home'" class="line1">
-      <span class="span">帮助</span>
-      <span class="span">隐私</span>
-      <span class="span">条款</span>
-    </div> -->
     <div class="line2">© 2019 银江股份有限公司版权所有</div>
   </div>
 </template>
