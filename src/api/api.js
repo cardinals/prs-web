d58fcbd08d9e1034160707b6480dbf502ee60c7c
wupeiwen <javapeiwen2010@gmail.com>
2019-06-13 16:38:59 +0800

    remove: 移除无效api接口定义

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
  * @Author: wupeiwen javapeiwen2010@gmail.com
  * @Date: 2018-09-18 10:19:23
  * @Last Modified by: wupeiwen javapeiwen2010@gmail.com
- * @Last Modified time: 2019-05-09 17:29:34
+ * @Last Modified time: 2019-06-13 16:34:18
  */
 //  引入httpUtil
 import { get, post } from '@/utils/httpUtil'
@@ -11,18 +11,12 @@ const baseUrl = '/SearchCase'
 
 // 首页最新动态数据
 export const latestNews = () => get(`${baseUrl}/HomePages`)
-// 列表页树状列表测试数据
-export const getTestData = () => get('searchList1.json')
 // 列表页搜索结果数据
 export const getListData = (params) => post(`${baseUrl}/SearchResults`, params)
-// 列表页头像数据
-export const getPhotoData = () => get('searchList3.json')
 // 人物信息页左边栏数据
 export const getbasicInfo = (params) => get(`${baseUrl}/PersonLabels`, params)
 // 人物信息页数据
 export const getPeopleInfo = (params) => get(`${baseUrl}/PersonInformations`, params)
-// 测试数据
-export const getTest = () => get('test.json')
 // 人物动态页数据
 export const getDynamic = (params) => post(`${baseUrl}/PersonActivities`, params)
 // 中文提示
