e2e90a37da1d48501c735cc0e5de1ee33a9c4e83
ZMM <zhang163mingmin@163.com>
2019-06-13 10:17:56 +0800

    fix: vuex中的header模块新增状态

--- src/store/modules/header.js
+++ src/store/modules/header.js
@@ -8,12 +8,19 @@ const state = {
   // 搜索类型
   searchType: 'people',
   // 搜索按钮点击次数
-  searchClick: 0
+  searchClick: 0,
+  selectClick: 0
 }
 
 const getters = {
   getSearchClick (state) {
     return state.searchClick
+  },
+  getSearchVal (state) {
+    return state.searchVal
+  },
+  getSelectClick (state) {
+    return state.selectClick
   }
 }
 
@@ -30,11 +37,17 @@ const mutations = {
   },
   changeSearchClick (state, num) {
     state.searchClick += num
+  },
+  changeSelectClick (state) {
+    state.selectClick++
   }
 }
 const actions = {
   changeSearchClick (ctx, num) {
     ctx.commit('changeSearchClick', num)
+  },
+  changeSelectClick (ctx) {
+    ctx.commit('changeSelectClick')
   }
 }
 
