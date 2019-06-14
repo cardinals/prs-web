4a7ff37efa20eca76a5afe25cac476f445967710
ZMM <zhang163mingmin@163.com>
2019-06-14 11:02:17 +0800

    modify:修改接口代理路由

--- vue.config.js
+++ vue.config.js
@@ -27,7 +27,7 @@ module.exports = {
     // 配置代理转发
     proxy: {
       '/SearchCase': {
-        target: 'http://192.168.22.63:8081',
+        target: 'http://192.168.22.137:8081',
         changeOrigin: true
       }
     },
