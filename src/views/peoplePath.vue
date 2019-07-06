8798f7424276414697bf842f0185fa812aeb9871
ZMM <zhang163mingmin@163.com>
2019-07-06 09:26:32 +0800

    add: 需求开发: 人物轨迹页面开发，引入地图组件，接口改动

--- src/views/peoplePath.vue
+++ src/views/peoplePath.vue
@@ -13,7 +13,13 @@
         <span class="name">时空轨迹</span>
         <dateCheck @dateReturn="dateReturn" :dateDefault="dateDefault" />
       </div>
-      <div class="pathMap"></div>
+      <div class="pathMap">
+        <mapview :map-config="mapConfig" :osm-config="osmConfig"
+          :map-types="mapTypes"
+          :heatmap="heatmap"
+          @pointClick="callback">
+        </mapview>
+      </div>
     </div>
     <div class="tableContainer">
       <div class="title">
@@ -28,9 +34,11 @@
         </el-table>
         <div class="page" v-if="allTableNum > 5">
           <el-pagination
+            small
             layout="prev, pager, next"
-            :page-size="5"
+            :page-size="pageSize"
             :total="allTableNum"
+            :current-page="currentPage"
             @current-change="currentChange">
           </el-pagination>
         </div>
@@ -47,11 +55,40 @@ export default {
   data () {
     return {
       dateDefault: '本年',
-      mapData: [],
-      tableDate: [],
+      tableData: [],
       nowDate: [],
       allTableNum: 0,
-      maptype: 'points'
+      currentPage: 1,
+      pageSize: 5,
+      maptype: 'points',
+      onlyAbnormal: false,
+      mapConfig: {
+        // <!-- 中心点 -->
+        center: [121.4198, 31.1043],
+        // <!-- 缩放等级 -->
+        zoom: 8.7,
+        // <!-- 视角俯视的倾斜角度 -->
+        pitch: 43,
+        // <!-- 地图的旋转角度 -->
+        bearing: 0
+      },
+      osmConfig: {
+        // <!-- osm地址 -->
+        osmUrl: 'http://192.168.22.88:8700',
+        // <!-- 地图样式 -->
+        backgroundStyle: 'Positron'
+      },
+      // <!-- 地图的可视化类型 -->
+      mapTypes: ['heatmap'],
+      // <!-- 热力图配置项 -->
+      heatmap: {
+        radius: 5,
+        weight: 1,
+        intensity: 1,
+        opacity: 0.8,
+        color: ['interpolate', ['linear'], ['heatmap-density'], 0, 'rgba(0, 0, 255, 0)', 0.2, 'royalblue', 0.4, 'cyan', 0.6, 'lime', 0.8, 'yellow', 1, 'red'],
+        data: []
+      }
     }
   },
   computed: {
@@ -65,35 +102,71 @@ export default {
       return this.$store.state.path.peopleName
     }
   },
+  watch: {
+    $route: {
+      handler: function (val, oldVal) {
+        this.init()
+      },
+      deep: true
+    },
+    onlyAbnormal (newVal, oldVal) {
+      if (newVal) {
+        apiParams.flag = '1'
+      } else {
+        apiParams.flag = '0'
+      }
+      this.getData()
+    }
+  },
   methods: {
-    ...mapActions('dynamic', {
+    ...mapActions('path', {
       changeShowMsg: 'changeShowMsg'
     }),
+    callback (data) {
+      console.log(data)
+    },
     dateReturn (date) {
       this.nowDate = date
     },
     onlyDanger () {
-
+      this.changeShowMsg(false)
+      this.onlyAbnormal = true
     },
     currentChange (pageNum) {
-
+      this.currentPage = pageNum
+      this.getTableData(this.currentPage)
+      console.log(this.heatmap.data.length)
     },
+    // 改变表格中不同风险预警等级文字的颜色
     dangerColor ({ row, column, rowIndex, columnIndex }) {
-      if (columnIndex === 3 && this.tableData.list[rowIndex].label === '潜在异常') {
+      if (columnIndex === 3 && this.tableData[rowIndex].label === '潜在异常') {
         return 'color: rgba(244,153,48,1)'
-      } else if (columnIndex === 3 && this.tableData.list[rowIndex].label !== '正常') {
+      } else if (columnIndex === 3 && this.tableData[rowIndex].label !== '正常') {
         return 'color: rgba(247,77,78,1)'
       }
+    },
+    getTableData (val) {
+      let temp = JSON.parse(JSON.stringify(this.heatmap.data))
+      this.tableData = temp.splice(this.pageSize * (val - 1), this.pageSize)
+    },
+    getData () {
+      personTrajectory(apiParams).then(res => {
+        this.heatmap.data = res.data.data
+        this.allTableNum = res.data.resultNum
+        this.currentPage = 1
+        this.getTableData(this.currentPage)
+      })
     },
     init () {
       apiParams.g_id = this.$route.params.personId
       apiParams.timestart = this.nowDate[0] === '1919-01-01' ? 'all' : this.nowDate[0]
       apiParams.timeend = this.nowDate[1]
-
-      personTrajectory(apiParams).then(res => {
-        this.tableData = res.data
-      })
+      apiParams.flag = this.$route.params.type === 'err' ? '1' : '0'
+      this.getData()
     }
+  },
+  created () {
+
   },
   mounted () {
     this.dateDefault = this.$route.params.type === 'err' ? '全部' : '本年'
@@ -112,6 +185,12 @@ export default {
   th {
     padding: 0;
     line-height: 42px;
+  }
+}
+.pathTable {
+  .page {
+    margin-top: 20px;
+    text-align: center;
   }
 }
 </style>
