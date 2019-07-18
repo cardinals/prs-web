bc32ebe4a55bc2c7ba3595ec14a449b6dbd97be7
ZMM <zhang163mingmin@163.com>
2019-07-18 09:12:15 +0800

    refactor:组件重构:将关系图组件重构,抽离成可复用的组件

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -10,7 +10,7 @@ import footerCom from '@/components/common/footer'
 import goTop from '@/components/common/goTop'
 import upsideDownRoll from '@/components/common/upsideDownRoll'
 import dateCheck from '@/components/common/dateCheck'
-import networkMap from '@/components/common/networkMap'
+import relationGraph from '@/components/common/relationGraph'
 import anchorPoint from '@/components/common/anchorPoint'
 
 export default {
@@ -20,7 +20,7 @@ export default {
     vue.component('goTop', goTop)
     vue.component('upsideDownRoll', upsideDownRoll)
     vue.component('dateCheck', dateCheck)
-    vue.component('relation', networkMap)
+    vue.component('relation', relationGraph)
     vue.component('anchorPoint', anchorPoint)
   }
 }
