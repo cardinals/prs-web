c8e653de93df9a4d4fb428451853c1c5cff02cb1
ZMM <zhang163mingmin@163.com>
2019-08-01 16:55:47 +0800

    modified:修正文字:人物信息页的档案从放改为档案存放

--- src/main.js
+++ src/main.js
@@ -9,7 +9,15 @@ import 'vue-mapboxgl-components'
 import 'vue-mapboxgl-components/lib/vue-mapboxgl-components.css'
 
 Vue.config.productionTip = false
-
+let ww = document.documentElement.offsetWidth
+ww = ww >= 1366 ? ww : 1366
+document.documentElement.style.fontSize = (ww / 1920) * 100 + 'px'
+// rem随屏幕变化适配
+window.onresize = () => {
+  let ww = document.documentElement.offsetWidth
+  ww = ww >= 1366 ? ww : 1366
+  document.documentElement.style.fontSize = (ww / 1920) * 100 + 'px'
+}
 Vue.use(ElementUI)
 Vue.use(common)
 
